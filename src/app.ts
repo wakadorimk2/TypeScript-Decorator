f75c1e15f447dfbe4b2333df8e1d202353049961
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 17:00:00 +0900

    数値の合計を返す関数を追加

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,11 @@ const coppiedPerson = { ...person };
 console.log(person);
 console.log(coppiedPerson);
 
-const add = (...numbers: number[]) => {};
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
 
 const addedNumbers = add(5, 10, 2, 3.7);
 console.log(addedNumbers);
