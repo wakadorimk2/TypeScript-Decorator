d22b413ad328622f2f949654fd8c903298c83f62
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 16:42:06 +0900

    関数addのデフォルト引数を追加

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ let age = 30;
 
 age = 29;
 
-const add = (a: number, b: number) => a + b;
+const add = (a: number, b: number = 1) => a + b;
 
 const printOutput: (output: string | number) => void = output => console.log(output);
 
-printOutput(add(2, 5));
+printOutput(add(2));
 
 const button = document.querySelector('button')
 
