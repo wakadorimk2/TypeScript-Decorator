4593d7692bb1b5fab1af8118c3b2242aa27f669d
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 14:18:44 +0900

    関数addを追加しました

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 const button = document.querySelector('button');
 
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2
+    }
+    return;
+}
+
 function clickHandler(message: string) {
     console.log('Button clicked!' + message);
 }
