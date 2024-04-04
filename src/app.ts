100817abc4d140c5703fc339691b1fc542a87128
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 16:31:35 +0900

    ボタンクリック時にイベントをログに出力するコードを追加

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,9 @@ const add = (a: number, b: number) => a + b;
 const printOutput: (output: string | number) => void = output => console.log(output);
 
 printOutput(add(2, 5));
+
+const button = document.querySelector('button')
+
+if (button) {
+  button.addEventListener('click', event => console.log(event));
+}
