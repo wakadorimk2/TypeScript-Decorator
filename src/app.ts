27ac0331575a8914f23e0796e4f049e3c6f54c16
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 14:28:11 +0900

    フォーマッタを追加

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-const button = document.querySelector('button');
+const button = document.querySelector("button");
 
 function add(n1: number, n2: number) {
-    if (n1 + n2 > 0) {
-        return n1 + n2
-    }
-    return;
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
 }
 
 function clickHandler(message: string) {
-    console.log('Button clicked!' + message);
+  console.log("Button clicked!" + message);
 }
 
 if (button) {
-    button.addEventListener('click', clickHandler.bind(null, 'You\'re welcome!'));
+  button.addEventListener("click", clickHandler.bind(null, "You're welcome!"));
 }
