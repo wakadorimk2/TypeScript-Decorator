adb28663e221d402c90ea9d43e502a9e00748567
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 14:09:49 +0900

    ボタンのクリックハンドラを追加

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 const button = document.querySelector('button');
 
+function clickHandler() {
+    console.log('Button clicked!');
+}
+
 if (button) {
     button.addEventListener('click', () => {
         console.log('Button clicked!');
