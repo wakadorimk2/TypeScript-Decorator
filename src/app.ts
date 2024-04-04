0d33e9d49a6e6d30a5617b29fa3dd9a62e052398
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-04 17:24:10 +0900

    前章のコードを削除

--- src/app.ts
+++ src/app.ts
@@ -3,32 +3,3 @@ const button = document.querySelector('button')
 if (button) {
   button.addEventListener('click', event => console.log(event));
 }
-
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  firstName: 'Max',
-  age: 30
-};
-
-const coppiedPerson = { ...person };
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-
-const [hobby1, hobby2] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-
-console.log(person, userName, age);
