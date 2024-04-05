d7eebfe2ad363008cd19466a8fa6c3b48b2af6eb
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-05 17:12:35 +0900

    Personインターフェースにuser1オブジェクトを追加

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,13 @@ interface Person {
 
   greet(phrase: string): void;
 }
+
+let user1: Person;
+
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet(phrase: string) {
+        console.log(phrase + ' ' + this.name);
+    }
+}
