11d0d3f19669c7401a845c6d21a9304ff08fa619
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-05 17:17:05 +0900

    インターフェース名を変更しました***

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
-interface Person {
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+let user1: Greetable;
 
 user1 = {
     name: 'Max',
