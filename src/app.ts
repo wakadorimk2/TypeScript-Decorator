d80a60f6a21435b49f33e04af7496325ea9bfdb0
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-05 17:19:54 +0900

    クラスPersonにgreetメソッドを追加しました

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ class Person implements Greetable {
     constructor(n: string) {
         this.name = n;
     }
+
+    greet(phrase: string): void {
+        console.log(phrase + ' ' + this.name);
+    }
 }
 
 let user1: Greetable;
