2bd4c9f57f2205f8a4859219bb728b8799c8e460
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-08 10:00:30 +0900

    AddFn型の実装を修正しました

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 interface AddFn {
-    (a: number, b: number): number
+  (a: number, b: number): number;
 }
 
 let add: AddFn;
 
 add = (n1: number, n2: number) => {
-    return n1 + n2;
-}
+  return n1 + n2;
+};
 
 interface Named {
-    readonly name?: string;
-    outputName?: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -18,23 +18,23 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-    name?: string;
-    age = 30;
+  name?: string;
+  age = 30;
 
-    constructor(n: string) {
-        if (n) {
-        this.name = n;
-        }
+  constructor(n: string) {
+    if (n) {
+      this.name = n;
     }
+  }
 
-    greet(phrase: string): void {
-        console.log(phrase + ' ' + this.name);
-    }
+  greet(phrase: string): void {
+    console.log(phrase + " " + this.name);
+  }
 }
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person("Max");
 
-user1.greet('Hello ! I am');
+user1.greet("Hello ! I am");
 console.log(user1);
