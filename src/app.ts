dc0596775cb141b4ea5170f34217930c7e79daef
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-08 12:08:21 +0900

    add関数に文字列の場合の処理を追加しました

--- src/app.ts
+++ src/app.ts
@@ -22,5 +22,8 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
 function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
   return a + b;
 }
