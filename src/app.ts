e271a9fcfef27c8676613392274e8f67c534084f
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 13:57:33 +0900

    新しいジェネリック関数を追加しました

--- src/app.ts
+++ src/app.ts
@@ -36,3 +36,7 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 };
 
 extractAndConvert({ name: 'Max' }, "name");
+
+class DataStorage {
+    private data = [];
+}
