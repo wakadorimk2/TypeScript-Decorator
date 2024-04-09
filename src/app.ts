5bb17ccf436be24aebec2cee71eb848735d6ab96
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 11:46:51 +0900

    新しいPromiseオブジェクトのメソッドを修正しました

--- src/app.ts
+++ src/app.ts
@@ -15,4 +15,4 @@ function merge(objA: object, objB: object) {
     return Object.assign(objA, objB);
 }
 
-console.log(merge({name: 'Max'}, {age: 30}));
+const mergedObj =  merge({name: 'Max'}, {age: 30});
