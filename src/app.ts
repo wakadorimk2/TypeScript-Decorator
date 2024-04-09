50da966614e804b306e1931f2bd28593b1e836c9
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 14:18:15 +0900

    createCourseGoal関数を追加しました

--- src/app.ts
+++ src/app.ts
@@ -80,3 +80,11 @@ interface CourseGoal {
   description: string;
   completeUntil: Date;
 }
+
+function createCourseGoal(title: string, description: string, date: Date) {
+  return {
+    title: title,
+    description: description,
+    completeUntil: date,
+  };
+}
