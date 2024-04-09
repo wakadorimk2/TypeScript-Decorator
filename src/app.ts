899162845f599725176dd7e7131111de0b90902e
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 14:00:21 +0900

    オブジェクトのマージ関数に新しいジェネリック関数を追加しました

--- src/app.ts
+++ src/app.ts
@@ -42,4 +42,16 @@ extractAndConvert({ name: "Max" }, "name");
 
 class DataStorage {
   private data = [];
+
+  addItem(item) {
+    this.data.push(item);
+  }
+
+  removeItem(item) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data]
+  }
 }
