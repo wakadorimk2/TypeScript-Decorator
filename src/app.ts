83014a389ab85c6ca9f07a2d24a14be6e2743a80
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 13:49:49 +0900

    オブジェクトのマージ関数に新しいジェネリック関数を追加しました

--- src/app.ts
+++ src/app.ts
@@ -35,4 +35,4 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
     return 'value: ' + obj[key];
 };
 
-extractAndConvert({}, "name");
+extractAndConvert({ name: 'Max' }, "name");
