b3f9d2768d827b12c3391ea529efa76101bfb59d
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 11:20:19 +0900

    新しいPromiseオブジェクトのコメントを追加しました

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-const names: Array<string> = [];
-names[0].length
+// const names: Array<string> = [];
+// names[0].length
 
-const promise = new Promise<number>((resolve, reject) => {
-    setTimeout(() => {
-        resolve(10);
-    }, 2000);
-});
+// const promise = new Promise<number>((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(10);
+//     }, 2000);
+// });
 
-promise.then(data => {
-    data.split(' ');
-});
+// promise.then(data => {
+//     data.split(' ');
+// });
