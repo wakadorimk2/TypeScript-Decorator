9f7df2d373c43cb00d4ee8ef8f068fd78b853e91
wakadorimk2 <wakadori.mk2@gmail.com>
2024-04-09 10:10:09 +0900

    add関数に新しい引数のオーバーロードを追加しました

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
 function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
     return a.toString() + b.toString();
